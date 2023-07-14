b46891137584b436df36f4bac0cbaff5bb6184ad
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-07-14 13:47:15 +0200

    Added dynamic filtering

--- src/main/java/com/socialmedia/controller/FilteringController.java
+++ src/main/java/com/socialmedia/controller/FilteringController.java
@@ -1,6 +1,10 @@
 package com.socialmedia.controller;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.socialmedia.domain.SomeBean;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +14,37 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean filtering() {
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue filtering() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+
+        SimpleBeanPropertyFilter filter =
+                SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+
+        FilterProvider filters =
+                new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
-    public List<SomeBean> filteringList() {
-        return List.of(new SomeBean("value1", "value2", "value3"),
-                        new SomeBean("value4", "value5", "value6"));
+    public MappingJacksonValue filteringList() {
+        List<SomeBean> someBeans = List.of(new SomeBean("value1", "value2", "value3"),
+                new SomeBean("value4", "value5", "value6"));
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBeans);
+
+        SimpleBeanPropertyFilter filter =
+                SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+
+        FilterProvider filters =
+                new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        mappingJacksonValue.setFilters(filters);
+        
+        return mappingJacksonValue;
     }
 }
--- src/main/java/com/socialmedia/domain/SomeBean.java
+++ src/main/java/com/socialmedia/domain/SomeBean.java
@@ -1,14 +1,14 @@
 package com.socialmedia.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private final String field1;
-    @JsonIgnore
     private final String field2;
     private final String field3;
 }
