efe6a723dbad06c55896042da613833beedc86d4
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-07-19 18:42:26 +0200

    User - added connection to H2 database

--- src/main/java/com/socialmedia/service/UserService.java
+++ src/main/java/com/socialmedia/service/UserService.java
@@ -2,46 +2,32 @@ package com.socialmedia.service;
 
 import com.socialmedia.domain.User;
 import com.socialmedia.domain.exception.UserNotFoundException;
+import com.socialmedia.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    private final static List<User> users = new ArrayList<>();
-
-    private static Integer userCount = 0;
-
-    static {
-        users.add(new User(++userCount, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(++userCount, "Eve", LocalDate.now().minusYears(24)));
-        users.add(new User(++userCount, "Jim", LocalDate.now().minusYears(20)));
-    }
+    private final UserRepository repository;
 
     public List<User> findAll() {
-        return users;
+        return repository.findAll();
     }
 
     public User findUser(Integer userId) {
-        return users.stream()
-                .filter(user -> user.getId().equals(userId))
-                .findFirst()
+        return repository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("User with id: " + userId + " was not found!"));
     }
 
     public User save(User user) {
-        User newUser = new User(
-                ++userCount,
-                user.getName(),
-                user.getBirthDate());
-        users.add(newUser);
-        return newUser;
+        return repository.save(user);
     }
 
     public void deleteById(Integer userId) {
-        users.removeIf(user -> user.getId().equals(userId));
+        repository.deleteById(userId);
     }
 }
